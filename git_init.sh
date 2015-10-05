706dbf100cca7ea1869cc8ccd774393910c9c163
dorbala <dorbala@usc.edu>
2015-10-04 23:08:45 -0700

    Delete git_init.sh

--- git_init.sh
+++
@@ -1,7 +0,0 @@
-git init
-git remote add origin http://github.com/dorbala/latte.git
-git remote -v
-git pull origin master
-git add *
-git -c "user.email=dorbala@usc.edu" -c "user.name=dorbala" commit -m "Source files for latte"
-git push origin master
